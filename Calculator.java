04ab94fa107d978ce29fd1a4ed8fbbbe73c97eb2
shriyansh19 <shriyanshkesharwani3162@gmail.com>
2025-01-07 12:01:18 +0530

    Added addition functionality

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,10 @@
-class Calculator {
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Welcome to my calculator!"); 
+        System.out.println("Welcome to my calculator!");
+        addition(5, 3); 
+    }
+
+    public static void addition(int a, int b) {
+        System.out.println("Addition: " + (a + b));
     }
 }
