0d3b9c243e5bf96ad10a6536dd0c5d4fd9deee6e
shriyansh19 <shriyanshkesharwani3162@gmail.com>
2025-01-07 12:23:40 +0530

    Added subtraction functionality

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,15 @@ public class Calculator {
     public static void main(String[] args) {
         System.out.println("Welcome to my calculator!");
         addition(5, 3); 
+        subtraction(5, 3);
     }
 
     public static void addition(int a, int b) {
         System.out.println("Addition: " + (a + b));
     }
+
+    public static void subtraction(int a, int b) {
+    	System.out.println("Subtraction: " + (a - b));
+    }
+
 }
